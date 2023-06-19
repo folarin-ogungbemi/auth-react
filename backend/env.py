2f1de670934ccd749b96b43ba921d8a5956dc247
Folarin Ogungbemi <ftogungbemi@gmail.com>
2023-06-19 23:25:40 +0200

    Update env.py

--- backend/env.py
+++ backend/env.py
@@ -1,5 +1,1 @@
-import os
 
-os.environ['DATABASE_PASSWORD'] = 'progmined22!'
-os.environ['EMAIL_PASSWORD'] = 'iusfbelvadrnzdjl'
-os.environ['SECRET_KEY'] = '8kj8f%!=l$4agyr^rk09+geleg&fezflrmxm^le4h&vxmty2'

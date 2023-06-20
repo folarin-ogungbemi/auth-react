7b1e5b098c467ddd2f30bda5ebb92f6d8717e267
folarin-ogungbemi <ftogungbemi@gmail.com>
2023-06-20 14:27:18 +0200

    Add components routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,35 @@
+import { 
+  BrowserRouter as Router,
+  Routes,
+  Route
+ } from 'react-router-dom';
 import React from 'react'
-import {BrowserRouter as Router, Route, Routes, Switch } from 'react-router-dom'
+
+import Home from './containers/Home'
+import Login from './containers/Login'
+import Signup from './containers/SignUp'
+import Activate from './containers/Activate'
+import ResetPassword from './containers/ResetPassword'
+import ResetPasswordConfirm from './containers/ResetPasswordConfirm'
+import Layout from './hocs/Layout'
 
 const App = () => {
-  <Router>
-    <Routes>
-      <switch>
-        
-      </switch>
-    </Routes>
+  return(
+    <Router>
+    <div>
+      <Layout>
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/login" element={<Login/>}/>
+          <Route exact path="/signup" element={<Signup/>} />
+          <Route exact path="/reset-password" element={<ResetPassword/>}/>
+          <Route path="/password/reset/confirm/:uid/:token" element={<ResetPasswordConfirm/>}/>
+          <Route path="/activate/:uid/:token" element={<Activate/>}/>
+        </Routes>
+      </Layout>
+    </div>
   </Router>
+  )
 }
 
 export default App
